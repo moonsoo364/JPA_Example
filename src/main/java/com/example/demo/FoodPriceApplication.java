77e641baae51633532572895a0080da5b4705fbf
aptx15hibr@naver.com <aptx15hibr@naver.com>
2024-02-14 21:01:02 +0900

    yaml 분리

---
+++ src/main/java/com/example/demo/FoodPriceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodPriceApplication.class, args);
+	}
+
+}
